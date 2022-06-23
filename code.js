4b9cc3bf969d6ac7b334677577fa367424b03c1b
bello <bellosoliu12@gmail.com>
2022-06-24 00:36:31 +0100

    first commit

---
+++ code.js
@@ -0,0 +1,7 @@
+const my_name = document.querySelector("#myname");
+const height = document.querySelector("#height");
+const country = document.querySelector("#country");
+
+my_name.textContent = "Bello Soliu";
+height.textContent = "1.85m";
+country.textContent = "Nigeria";
